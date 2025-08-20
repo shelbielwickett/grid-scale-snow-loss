1b98e83da776285eeb8e2f88642102c6c4b38b9b
Shelbie Wickett <shelbiedavis@gmail.com>
2025-08-19 18:35:48 -0700

    Updated to use portable file paths for GitHub sharing

--- Existing Site Analysis/Existing 05 - Max Loss Hour Visual.py
+++ Existing Site Analysis/Existing 05 - Max Loss Hour Visual.py
@@ -1,3 +1,12 @@
+"""
+@author: shelbiedavis1
+This code plots the Maximum Loss Hour for each year or season for all sites. It
+also plots the tracking and fixed tilt systems separately.
+The axes need to be updated to adapt to the input data. Right now, the axes are
+configured for plotting all years 2013-2022.
+Updated on 8/19/25 to use portable file paths for GitHub sharing
+"""
+
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
@@ -6,18 +15,22 @@ import os
 from pathlib import Path
 
 # Parameters
-start_year = 2013
+start_year = 2022
 end_year = 2022
 title = 'Eastern Interconnect'
 #months = [12, 1, 2]  # Winter months
 months = [1,2,3,4,5,6,7,8,9,10,11,12]
 yearly_summary = []  # To store yearly summary results
 
+# === EDIT THIS PATH ===
+BASE_DIR = Path("/Volumes/Wickett SSD/Snow_Loss_Project")
+
 
 # Iterate through each year
 for year in range(start_year, end_year + 1):
-    file = f"{year} {title} Analysis"
-    with open(f'/Users/shelbiedavis1/Multi-State Simulation/Project json files/{file}.json') as f:
+    JSON_DIR = BASE_DIR / "Project json files"
+    file = f"{year} {title} Analysis.json"
+    with open(JSON_DIR / file) as f:
         site_dict = json.load(f)
 
     # Temporary storage for all site losses
@@ -51,7 +64,7 @@ for year in range(start_year, end_year + 1):
         # Load No Snow data
         try:
             SAM_no_snow = pd.read_csv(
-                f'/Volumes/Wickett SSD/PySAM_Results/Existing_Sites_Results/No_Snow/{year} SAM Results/{site_data["Project Name"]}_{year}_Results.csv'
+                BASE_DIR / f'PySAM_Results_UTC/Existing_Sites_Results_UTC/No_Snow/{year} SAM Results/{site_data["Project Name"]}_{year}_Results.csv'
                 )
         except Exception as e:
             print(f"Error: {e}")
@@ -62,38 +75,13 @@ for year in range(start_year, end_year + 1):
         # Load Snow data
         try:
             SAM_snow = pd.read_csv(
-                f'/Volumes/Wickett SSD/PySAM_Results/Existing_Sites_Results/Roof_Slide_Coeff/{year} SAM Results/{site_data["Project Name"]}_{year}_Results.csv'
+                BASE_DIR / f'PySAM_Results_UTC/Existing_Sites_Results_UTC/Roof_Slide_Coeff/{year} SAM Results/{site_data["Project Name"]}_{year}_Results.csv'
                 )
         except Exception as e:
             print(f"Error: {e}")
         SAM_snow['UTC'] = pd.to_datetime(SAM_snow['UTC'])
         SAM_snow = SAM_snow[SAM_snow['UTC'].dt.month.isin(months)]
         
-        # # Define UTC hourly index for full year
-        # full_index = pd.date_range(
-        #     start=f'{year}-01-01 00:30:00',
-        #     end=f'{year}-12-31 23:30:00',
-        #     freq='h',
-        #     tz='UTC'
-        #     )
-
-        # try:
-        #     # Load No Snow data
-        #     no_snow_path = Path(f'PySAM_Results_UTC/Existing_Sites_Results/No_Snow/{year} SAM Results/{site_data["Project Name"]}_{year}_Results.csv')
-        #     SAM_no_snow = pd.read_csv(no_snow_path, index_col=0, parse_dates=True)
-        #     SAM_no_snow.index.name = 'Datetime'
-        #     SAM_no_snow = SAM_no_snow.reindex(full_index)
-        #     SAM_no_snow['DC no snow Power [W]'] = SAM_no_snow['DC Inverter Input Power [W]'].fillna(0)
-        #     SAM_no_snow = SAM_no_snow[SAM_no_snow.index.month.isin(months)]
-
-        #     # Load Snow data
-        #     snow_path = Path(f'PySAM_Results_UTC/Existing_Sites_Results/Roof_Slide_Coeff/{year} SAM Results/{site_data["Project Name"]}_{year}_Results.csv')
-        #     SAM_snow = pd.read_csv(snow_path, index_col=0, parse_dates=True)
-        #     SAM_snow.index.name = 'Datetime'
-        #     SAM_snow = SAM_snow.reindex(full_index)
-        #     SAM_snow['DC snow Power [W]'] = SAM_snow['DC Inverter Input Power [W]'].fillna(0)
-        #     SAM_snow = SAM_snow[SAM_snow.index.month.isin(months)]
-           
         try:            
             # Calculate loss
             SAM_snow['Loss'] = SAM_no_snow['DC Inverter Input Power [W]'] - SAM_snow['DC Inverter Input Power [W]']
@@ -187,7 +175,7 @@ width = 0.15
 offsets = [-2.5*width, -1.5 * width, -0.5 * width, 0.5 * width, 1.5 * width, 2.5 * width]
 
 # Create the bar plot with updated date formatting
-fig, ax = plt.subplots(figsize=(14, 6))
+fig1, ax = plt.subplots(figsize=(14, 6))
 bars_total = ax.bar(x + offsets[0], total_gen, width, label='Total Power', color='#E69F00')
 bars_total_snow = ax.bar(x + offsets[1], total_gen_snow, width, label = 'Total Power w/ Snow Cover', color = '#FFCD66', hatch='\\')
 bars_tracking = ax.bar(x + offsets[2], tracking_gen, width, label='Tracking Power', color='#009E73')
@@ -195,25 +183,6 @@ bars_tracking_snow = ax.bar(x + offsets[3], tracking_gen_snow, width, label = 'T
 bars_fixed = ax.bar(x + offsets[4], fixed_gen, width, label='Fixed Tilt Power', color='#56B4E9')
 bars_fixed_snow = ax.bar(x + offsets[5], fixed_gen_snow,width, label = 'Fixed Tilt Power w/ Snow Cover', color = '#A8DDF5', hatch='\\' )
 
-# Add the max loss date above the bars
-#for i, date in enumerate(max_loss_dates):
-#    formatted_date = date.strftime('%m/%d/%y')
-#    formatted_time = date.strftime('%H:%M')
-#    rounded_total_loss = round(total_losses,3)
-#    ax.text(x[i], max(total_losses[i], tracking_losses[i], fixed_tilt_losses[i])+.01, formatted_date + '\n' + formatted_time + '\n'+str(rounded_total_loss[i]), 
-#            ha='center', va='bottom', fontsize=9)
-
-# # Add text above "Tracking Sites" bars for the loss
-# for i, (bar_without_snow, bar_with_snow) in enumerate(zip(bars_total, bars_total_snow)):
-#     if summary_df['Total Power Generation at Max Loss [W]'][i] != 0:
-#         loss = total_losses[i]
-#         formatted_date = max_loss_dates[i].strftime('%m/%d/%y')
-#         formatted_time = max_loss_dates[i].strftime('%H:%M')
-#         x_position = bar_without_snow.get_x() + bar_without_snow.get_width() / 1
-#         y_position = bar_without_snow.get_height() +1  # Place text 10 units above the bar
-#         ax.text(x_position, y_position, f'Total Loss: {loss:.0f} MW\n{formatted_date} {formatted_time}', ha='center', va='bottom', fontsize=9, color='black')
-#     else:
-#         continue
 # %%
 # Customize the plot
 ax.set_xlabel('Year', fontsize = 14)
@@ -249,10 +218,13 @@ for i, (bar_without_snow, bar_with_snow) in enumerate(zip(bars_total, bars_total
     
     ax.text(x_center, bracket_top+.1, f'Max Loss\nHour:\n{formatted_date} {formatted_time}\nTotal Loss:\n{total_loss:.1f} GW\n({total_percent_loss:.1f}%)', ha='center', va='bottom', fontsize=10, color='black')
 
+fig_dir= BASE_DIR / 'Figures'
+folder = fig_dir.parent
+folder.mkdir(parents=True, exist_ok=True)
 # Show the plot
 plt.tight_layout()
-#fig.savefig("/Users/shelbiedavis1/Multi-State Simulation/Figures/Test.png", dpi=300, bbox_inches="tight")
-plt.show()
+fig1.savefig(BASE_DIR / 'Figures/Max Loss Hour Figure 1.pdf', format='pdf', bbox_inches='tight')
+#plt.show()
 
 # %% Stacked plot
 
@@ -270,7 +242,7 @@ other_offsets = [.5 * spacing, 1.5 * spacing]
 
 
 # Create the bar plot with updated date formatting
-fig, ax = plt.subplots(figsize=(14, 6))
+fig2, ax = plt.subplots(figsize=(14, 6))
 bars_total = ax.bar(
     x + all_offsets[0], 
     total_gen, 
@@ -323,25 +295,6 @@ bars_fixed_snow = ax.bar(
     bottom = tracking_gen_snow
     )
 
-# Add the max loss date above the bars
-#for i, date in enumerate(max_loss_dates):
-#    formatted_date = date.strftime('%m/%d/%y')
-#    formatted_time = date.strftime('%H:%M')
-#    rounded_total_loss = round(total_losses,3)
-#    ax.text(x[i], max(total_losses[i], tracking_losses[i], fixed_tilt_losses[i])+.01, formatted_date + '\n' + formatted_time + '\n'+str(rounded_total_loss[i]), 
-#            ha='center', va='bottom', fontsize=9)
-
-# # Add text above "Tracking Sites" bars for the loss
-# for i, (bar_without_snow, bar_with_snow) in enumerate(zip(bars_total, bars_total_snow)):
-#     if summary_df['Total Power Generation at Max Loss [W]'][i] != 0:
-#         loss = total_losses[i]
-#         formatted_date = max_loss_dates[i].strftime('%m/%d/%y')
-#         formatted_time = max_loss_dates[i].strftime('%H:%M')
-#         x_position = bar_without_snow.get_x() + bar_without_snow.get_width() / 1
-#         y_position = bar_without_snow.get_height() +1  # Place text 10 units above the bar
-#         ax.text(x_position, y_position, f'Total Loss: {loss:.0f} MW\n{formatted_date} {formatted_time}', ha='center', va='bottom', fontsize=9, color='black')
-#     else:
-#         continue
 
 # Customize the plot
 ax.set_xlabel('Year', fontsize = 16)
@@ -377,9 +330,9 @@ for i, (bar_without_snow, bar_with_snow) in enumerate(zip(bars_total, bars_total
     
     ax.text(x_center, bracket_top+.1, f'Max Loss\nHour:\n{formatted_date} {formatted_time}\nTotal Loss:\n{total_loss:.1f} GW\n({total_percent_loss:.1f}%)', ha='center', va='bottom', fontsize=13, color='black')
 
+
 # Show the plot
 plt.tight_layout()
-#fig.savefig("/volumes/wickett SSD/maximum_power_loss_from_snow_1_hour_eastern_interconnect_stacked.png", dpi=300, bbox_inches="tight")
-#fig.savefig("/Users/shelbiedavis1/Multi-State Simulation/Figures/Test2.pdf", format='pdf', bbox_inches="tight")
-fig.savefig("/Volumes/Wickett SSD/Figures/Fig_3.pdf", format='pdf', bbox_inches='tight')
+
+fig2.savefig(BASE_DIR / 'Figures/Max Loss Hour Figure 2.pdf', format='pdf', bbox_inches='tight')
 plt.show()
